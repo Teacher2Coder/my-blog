76d21a67d625d64009ce5b505e3b7c39b7a403c3
Ethan Owens <ethan@Ethans-MacBook-Pro.local>
2024-07-25 10:28:44 -0500

    debugged storage issue

--- assets/js/form.js
+++ assets/js/form.js
@@ -10,44 +10,39 @@ const contentInput = document.querySelector('#content');
 // Retreives current storage if any
 let blogStorage = JSON.parse(localStorage.getItem('blogs'));
 
+// If storage is empty, create this object and store it
+// This first obbject will NOT be displayed on the blogs page
+if (blogStorage == null) {
+    let blogStore = {
+        postTitles: ["Hidden Title"],
+        postAuthors: ["Hidden Author"],
+        postContent: ["Hidden Content"],
+    }
+    localStorage.setItem('blogs', JSON.stringify(blogStore));
+} ;
+
 // When the 'submit button is clicked, the user's input is collected and pushed into the variables above,
 // then it is stringified and added to local storage.
 submitButton.addEventListener('click', function (event) {
     // Prevents default action
     event.preventDefault();
 
-    // If no storage, create a new object to be used later
-    // If there is storage, use current storage and add to it
-    if (blogStorage == null) {
-        let newBlogStore = {
-            titleStore: [],
-            authorStore: [],
-            contentStore: [],
-        } ;
-        newBlogStore;
-        console.log("It is null");
-    } else {
-        let blogStore = blogStorage;
-        blogStore;
-        console.log("It is not null");
-    } ;
+    // Retrieves blogs from local storage
+    let blogStorage = JSON.parse(localStorage.getItem('blogs'));
 
-    if (usernameInput.value == "") {
-        message.textContent = "Please enter username."
-    } else if (titleInput.value == "") {
-        message.textContent = "Please enter a title for your post."
-    } else if (contentInput.value == "") {
-        message.textContent = "Please enter content for your post."
-    } else {
-        // collect user input and store as variable
-        blogStorage.titleStore.push(titleInput.value);
-        blogStorage.authorStore.push(usernameInput.value);
-        blogStorage.contentStore.push(contentInput.value);
+    // Push user inputs into object
+    blogStorage.postTitles.push(titleInput.value);
+    blogStorage.postAuthors.push(usernameInput.value);
+    blogStorage.postContent.push(contentInput.value);
+
+    // Stores User inputs as a string
+    localStorage.setItem('blogs', JSON.stringify(blogStorage));
     
-        // Stores blog post data as strings in local storage
-        localStorage.setItem('blogs', JSON.stringify(blogStore));
-        // Takes user to the blog page
-        // UNCOMMENT LINE BELOW BEFORE SUBMISSION!!!!!!
-        // window.location.assign("blog.html")
-    } ;
+    // Clears input fields
+    usernameInput.value = "";
+    titleInput.value = "";
+    contentInput.value = "";
+
+    // Takes user to the blog page
+    window.location.assign("blog.html");
 } ) ;
